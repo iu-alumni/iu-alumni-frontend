7fd90df257d62305c4c4839818aae3202798ca67
Jaffar Totanji <94690848+KuronoSangatsu7@users.noreply.github.com>
2025-02-25 21:30:53 +0300

    feat: init nuxt and deps (#3)

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { useCounterStore } from "@/store/counter";
+
+const counter = useCounterStore();
+</script>
+
+<template>
+    <p>Count: {{ counter.count }}</p>
+    <Button @click="counter.increment">Click me</Button>
+</template>
