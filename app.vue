1f228458691b55e1954c38ca19ffbb0d2b3f36b3
Jaffar Totanji <94690848+KuronoSangatsu7@users.noreply.github.com>
2025-04-30 09:11:32 +0300

    feat: Excel upload, global error handling (#25)

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import Toaster from '@/components/ui/toast/Toaster.vue'
 </script>
 
 <template>
+  <ClientOnly>
+    <Toaster />
+  </ClientOnly>
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
