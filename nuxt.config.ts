58f9a62e2e3acaf2c3f93b18cb9c6f3e3f8caafc
escalopa <ahmed.helaly.dev@gmail.com>
2026-03-01 14:08:02 +0300

    feat: use localhost as backoff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     telemetry: false,
     runtimeConfig: {
         public: {
-            apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://www.xdkomel.ru/",
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8080/",
         },
     },
     modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
