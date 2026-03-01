4227b2a5edf4905bca3e97f989abf4df08cfb62d
escalopa <ahmed.helaly.dev@gmail.com>
2026-03-01 14:05:16 +0300

    feat: use dynamic backend url

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://www.xdkomel.ru/",
+    baseURL: useRuntimeConfig().public.apiBase,
     timeout: 30000,
 });
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     telemetry: false,
+    runtimeConfig: {
+        public: {
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://www.xdkomel.ru/",
+        },
+    },
     modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
